5c72ccc9aad44dff9ec4156fe1a46c7457e16f7e
Florian Berger <mail@florian-berger.de>
2015-12-31 12:41:05 +0100

    - Adding Item class.

--- openmediarepository.py
+++ openmediarepository.py
@@ -12,6 +12,30 @@
    >>> a = omr.Accounts()
    >>>
 
+
+   ## Repository Items
+
+   Media repository items are key-value storages with a mandatory set
+   of keys. They can either be represented by a dict or by an object
+   who responds accordingly to attribute queries.
+
+   The identifier of an item is the hex digest of the Whirlpool hash
+   of its content.
+
+   >>> import hashlib
+   >>> hash = hashlib.new("whirlpool", bytes("<svg><!-- Test 1 --></svg>", encoding = "utf8"))
+   >>> test_item_1 = {"identifier" : hash.hexdigest()}
+   >>> class TestItem:
+   ...     pass
+   >>> test_item_2 = TestItem()
+   >>> hash = hashlib.new("whirlpool", bytes("<svg><!-- Test 2 --></svg>", encoding = "utf8"))
+   >>> test_item_2.identifier = hash.hexdigest()
+
+   For convenience, there is an item class.
+
+   >>> import io
+   >>> test_item_3 = omr.Item(io.BytesIO(bytes("<svg><!-- Test 3 --></svg>", encoding = "utf8")))
+
 """
 
 # This file is part of OpenMediaRepository.
@@ -32,6 +56,7 @@
 # Work started on 31. Dec 2015.
 
 import logging
+import hashlib
 import cherrypy
 
 VERSION = "0.1.0"
@@ -47,6 +72,20 @@ PORT = 8000
 THREADS = 10
 AUTORELOAD = False
 
+class Item:
+    """Convenience class, representing a repository item.
+    """
+
+    def __init__(self, fp):
+        """Initialise.
+           fp is a binary mode filepointer pointing to the media file
+           to be represented.
+        """
+
+        self.identifier = hashlib.new("whirlpool", fp.read()).hexdigest()
+
+        return
+        
 class Repository:
     """Represent media items, and provide access.
     """
